c41a8efa9a248f5976b4ea66dedae732e8234141
SHETHA <shethabindhcpsalil@gmail.com>
2024-07-24 00:05:44 +0530

    final commit

--- api/sendemail.php
+++ api/sendemail.php
@@ -7,10 +7,10 @@ include "php/Exception.php";
 include "php/PHPMailer.php";
 include "php/SMTP.php";
 
-define("SMTP_EMAIL", "vivek.hashroot@gmail.com");
-define("SMTP_PASSWORD", "hhuyzvofvsggypwk");
-define("RECIPIENT_NAME", "Vivek Sahu");
-define("RECIPIENT_EMAIL", "vivek.hashroot@gmail.com");
+define("SMTP_EMAIL", "sales@kenzburaq.com");
+define("SMTP_PASSWORD", "bflsotpzleeyhjov");
+define("RECIPIENT_NAME", "Kenz Buraq");
+define("RECIPIENT_EMAIL", "sales@kenzburaq.com");
 
 // Read the form values
 $success = false;
